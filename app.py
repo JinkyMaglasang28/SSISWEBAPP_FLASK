160741f35974df18866d2888ea8dc78c5fe8a236
JinkyMaglasang28 <81910904+JinkyMaglasang28@users.noreply.github.com>
2022-01-23 20:50:30 +0800

    changes

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 app.secret_key = "flash message"
-
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'root'
--- run.py
+++ run.py
@@ -1,6 +1,10 @@
-from SSIS import app
+from SSIS import create_app
+from dotenv import load_dotenv
 
+app = create_app()
 
+load_dotenv('.env')
 
-if __name__=="__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
+    
