e680513284b86d296513df2aeeb6da19808fc4e7
JinkyMaglasang28 <81910904+JinkyMaglasang28@users.noreply.github.com>
2022-01-27 09:38:03 +0800

    Updated File Structure

--- SSIS/views/students/routes.py
+++ SSIS/views/students/routes.py
@@ -16,7 +16,7 @@ def Index():
 
 @student.route('/insert', methods = ['POST'])
 def insert():
-    stud_id = request.form['stud_id']
+    stud_id = (request.form['stud_id']).strip()
     file = request.files.get('file')
     fname = request.form['fname']
     lname = request.form['lname']
@@ -25,7 +25,9 @@ def insert():
     gender = request.form['gender']
     if request.method == "POST":
         flash("Data Inserted Successfully")
+        sample="ssis/{}".format(stud_id)
         upload(file.read(), public_id="ssis/{}".format(stud_id))
+
         cur = mysql.connection.cursor()
         cur.execute("INSERT INTO student_list (stud_id, fname, lname, course, year_lvl, gender) VALUES (%s, %s, %s, %s, %s, %s)",
                     (stud_id, fname, lname, course, year_lvl, gender))
@@ -51,6 +53,11 @@ def update():
         year_lvl = request.form['year_lvl']
         gender = request.form['gender']
         cur = mysql.connection.cursor()
+        file = request.files.get('file')
+        if file:
+            file_name = 'ssis/' + stud_id
+            destroy(file_name)
+            upload(file.read(), public_id="ssis/{}".format(stud_id.strip()))
         cur.execute("""UPDATE student_list SET fname=%s, lname=%s, course=%s, year_lvl=%s, gender=%s WHERE stud_id=%s""",
                     (fname, lname, course, year_lvl, gender, stud_id))
         flash("Data updated successfully")
